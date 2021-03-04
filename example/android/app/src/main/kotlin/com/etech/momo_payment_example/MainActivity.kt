c4f0e175cd36ed528d229ea73065ad496e4c732e
MacAnhTu <anhtucopyright@gmail.com>
2021-03-04 16:59:31 +0700

    .

--- example/android/app/src/main/kotlin/com/etech/momo_payment_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/etech/momo_payment_example/MainActivity.kt
@@ -3,3 +3,4 @@ package com.etech.momo_payment_example
 import io.flutter.embedding.android.FlutterActivity;
 
 class MainActivity: FlutterActivity() {}
+// com.etech.momo_payment.MomoPaymentPlugin.registerWith(shimPluginRegistry.registrarFor("com.etech.momo_payment.MomoPaymentPlugin"));
